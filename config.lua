186c809ab72e4ba6b434b4c246d227ae17391c21
Illinuz <snellemany@gmail.com>
2024-11-29 15:01:41 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+Config.InventoryType = "qb" -- Options: "qb", "ox", "qs"
+
+Config.Rewards = {
+    {type = "item", name = "phone", amount = 1, chance = 10},
+    {type = "item", name = "lockpick", amount = 2, chance = 30},
+    {type = "money", amount = 1000, chance = 40},
+    {type = "money", amount = 2000, chance = 20}
+}
